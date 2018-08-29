e108bfb130f20355032172297f217f02904edd7c
Corey Worrell <corey@coreyworrell.com>
2018-08-29 12:52:25 -0700

    Fallback for Composer autoload

--- plugin.php
+++ plugin.php
@@ -10,11 +10,15 @@
 define('FIRE_PATH', __DIR__.'/');
 define('FIRE_URL', WP_PLUGIN_URL.basename(__DIR__).'/');
 
+// Fallback autoloader for when Composer not setup
 include __DIR__.'/src/Foundation/Psr4Autoloader.php';
 
 $loader = new Fire\Foundation\Psr4Autoloader;
 $loader->register();
-$loader->addNamespace('Fire', __DIR__.'/src');
+
+if ( ! class_exists('Fire\Foundation\Fire')) {
+    $loader->addNamespace('Fire', __DIR__.'/src');
+}
 
 add_action('muplugins_loaded', function () use ($loader) {
     do_action('fire/autoload/required', $loader);
