5268366bc4cf1e9fdde6588750cfc8591b2e701e
Corey Worrell <cworrell@emrl.com>
2021-06-25 13:21:22 -0700

    Improve `Fire\Post\Type\Support` tests

--- tests/Post/Type/SupportTest.php
+++ tests/Post/Type/SupportTest.php
@@ -7,42 +7,66 @@ namespace Fire\Tests\Post\Type;
 use Fire\Post\Type\Support;
 use Fire\Tests\TestCase;
 
-use function Brain\Monkey\Functions\expect;
+use function Brain\Monkey\Functions\when;
 
 final class SupportTest extends TestCase
 {
     public function testAddSupport(): void
     {
+        $support = [];
+
+        when('add_post_type_support')
+            ->alias(function (string $type, string $feature, array $args = []) use (&$support): void {
+                $support[$type] ??= [];
+                $support[$type][$feature] = $args ?: true;
+            });
+
         $type = 'post';
         $test1 = 'test1';
         $test2 = 'test2';
         $test2_args = ['testvalue'];
         $supports = [$test1, $test2 => $test2_args];
 
-        expect('add_post_type_support')
-            ->once()
-            ->with($type, $test1);
-
-        expect('add_post_type_support')
-            ->once()
-            ->with($type, $test2, $test2_args);
-
         (new Support($type, $supports))();
+
+        $this->assertSame(
+            [
+                $type => [
+                    $test1 => true,
+                    $test2 => $test2_args,
+                ],
+            ],
+            $support,
+        );
     }
 
     public function testRemoveSupport(): void
     {
         $type = 'post';
+
+        $support = [
+            $type => [
+                'test1' => true,
+                'test2' => ['test' => 'another'],
+                'test3' => true,
+            ],
+        ];
+
         $supports = ['test1', 'test2'];
 
-        expect('remove_post_type_support')
-            ->once()
-            ->with($type, $supports[0]);
-
-        expect('remove_post_type_support')
-            ->once()
-            ->with($type, $supports[1]);
+        when('remove_post_type_support')->alias(function (string $type, string $feature) use(&$support): void {
+            unset($support[$type][$feature]);
+        });
 
         (new Support($type, $supports))->remove();
+
+        $this->assertSame(
+            [
+                $type => [
+                    'test3' => true,
+                ]
+            ],
+            $support,
+        );
     }
 }
